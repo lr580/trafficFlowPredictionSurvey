86fb32d88d60ce43991ad01f0f52f5ee8d8b68d2
lr580 <2277379149@qq.com>
2024-09-29 23:59:19 +0800

    添加了学习笔记

--- lc2073.py
+++
@@ -1,6 +0,0 @@
-from typing import *
-class Solution:
-    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        ans = sum([min(tickets[i], tickets[k]) for i in range(k)])
-        ans += sum([min(tickets[i], tickets[k]-1) for i in range(k+1, len(tickets))])
-        return ans
